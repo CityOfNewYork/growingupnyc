4dac3d8756b434e9b963a4d9c632916042a0dd12
Kelli Monahan <kmonahan@bluestatedigital.com>
2016-07-01 10:09:00 -0500

    Remove jQuery until it's needed

--- wp-content/themes/guny/functions.php
+++ wp-content/themes/guny/functions.php
@@ -60,7 +60,9 @@ class GunySite extends TimberSite {
     if (!is_admin()) {
       wp_deregister_script('jquery');
       wp_enqueue_script( 'modernizr', get_template_directory_uri() . '/assets/js/modernizr.js', array(), '3.0.0', false );
-      wp_enqueue_script( 'jquery', get_template_directory_uri() . '/src/js/vendor/jquery.js', array(), '2.1.14', false );
+      if (is_page_template( 'template-search.php') ) {
+        wp_enqueue_script( 'jquery', get_template_directory_uri() . '/src/js/vendor/jquery.js', array(), '2.1.14', false );
+      }
       wp_enqueue_script( 'site-js', get_template_directory_uri() . '/assets/js/source.js', array(), '1.0.0', true );
     }
   }

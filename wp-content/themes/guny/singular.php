f1c5997c04456c71b5967ad70d3b72090fa8678a
Kelli Monahan <kmonahan@bluestatedigital.com>
2016-07-06 11:35:34 -0500

    Add layout object

--- wp-content/themes/guny/singular.php
+++ wp-content/themes/guny/singular.php
@@ -12,31 +12,4 @@ $templates = array( 'single-' . $post->ID . '.twig', 'single-' . $post->post_typ
 
 $context['post'] = $post;
 
-if ( is_front_page() ) {
-  array_unshift( $templates, 'home.twig' );
-
-  $context['testimonials'] = Timber::get_posts( array(
-    'post_type' => 'testimonial',
-    'numberposts' => 2,
-    'post_status' => 'publish'
-  ) );
-
-  $context['recent_posts'] = Timber::get_posts( array(
-    'post_type' => 'post',
-    'numberposts' => 5,
-    'post_status' => 'publish'
-  ) );
-} else {
-  $sidebar_context = array();
-  // Add any data to the sidebar here
-  $sidebar_slug = guny_get_sidebar_slug( $post );
-  if ( is_active_sidebar( 'sidebar_' . $sidebar_slug ) ) {
-    $sidebar_context['widgets'] = Timber::get_widgets('sidebar_' . $sidebar_slug);
-  } else {
-    $sidebar_context['widgets'] = Timber::get_widgets('sidebar');
-  }
-
-  $context['sidebar'] = Timber::get_sidebar(['sidebar-' . $sidebar_slug . '.twig', 'sidebar.twig'], $sidebar_context);
-}
-
 Timber::render( $templates, $context );

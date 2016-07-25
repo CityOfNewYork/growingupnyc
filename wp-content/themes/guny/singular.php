a244b899caf6471151da86c579a7b9bd57260e68
Kelli Monahan <kmonahan@bluestatedigital.com>
2016-07-25 16:01:17 -0500

    Add Age Landing hero (desktop)

--- wp-content/themes/guny/singular.php
+++ wp-content/themes/guny/singular.php
@@ -7,6 +7,10 @@
 
 $context = Timber::get_context();
 $post = Timber::get_post();
+$age_groups = $post->terms('age_group');
+if( $age_groups ) {
+  $post->age_group = $age_groups[0];
+}
 
 $templates = array( 'single-' . $post->ID . '.twig', 'single-' . $post->post_type . '.twig', 'single.twig' );
 

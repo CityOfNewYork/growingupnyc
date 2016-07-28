972400821f5f5b9eac2443e23d439752e1fd0153
Traci Potocnik <traci.potocnik@gmail.com>
2016-07-28 14:51:49 -0400

    Added no results text and updated how the pager pulls in the fields

--- wp-content/themes/guny/tribe-events/month.php
+++ wp-content/themes/guny/tribe-events/month.php
@@ -9,6 +9,7 @@ $context = Timber::get_context();
 while ( tribe_events_have_month_days() ) : tribe_events_the_month_day();
   $day = tribe_events_get_current_month_day();
 
+
   if(!empty($day['events']->posts) && $day['month'] == 0) {
 
     // Empty array
@@ -25,18 +26,10 @@ while ( tribe_events_have_month_days() ) : tribe_events_the_month_day();
   }
 endwhile;
 
-$context['post_filters'] = tribe_events_get_filters();
-
-$post_views = tribe_events_get_views();
-
-foreach ($post_views as $key => $view) {
-  $view['selected'] = tribe_is_view($view['displaying']);
-  $post_views[$key] = $view;
-}
-
-$context['post_views'] = $post_views;
-
-$context['current_url'] = tribe_events_get_current_filter_url();
+$tribe_ecp = Tribe__Events__Main::instance();
+$context['prev_month_url'] = $tribe_ecp->getLink( 'month', $tribe_ecp->previousMonth( tribe_get_month_view_date() ), null );
+$context['next_month_url'] = $tribe_ecp->getLink( 'month', $tribe_ecp->nextMonth( tribe_get_month_view_date() ), null );
+$context['current_month_text'] = date('F', strtotime( tribe_get_month_view_date() ));
 
 $templates = array( 'list-events.twig', 'index.twig' );
 

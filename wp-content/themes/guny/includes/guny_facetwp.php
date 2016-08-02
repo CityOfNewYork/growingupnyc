a9905d9c119ed64a2306051e4b62a401b87590bc
Kelli Monahan <kmonahan@bluestatedigital.com>
2016-08-02 15:10:45 -0500

    Fix issue where pagination didn't display if there were only 2 pages

--- wp-content/themes/guny/includes/guny_facetwp.php
+++ wp-content/themes/guny/includes/guny_facetwp.php
@@ -42,7 +42,7 @@ function guny_facetwp_pager_html( $output, $params ) {
   if ( 1 <= ( $page - 1 ) ) {
       $output .= '<a class="facetwp-page button--outline button--outline--gray alignleft" data-page="' . ($page - 1) . '">Previous</a>';
   }
-  if ( $total_pages > ( $page + 1 ) ) {
+  if ( $total_pages >= ( $page + 1 ) ) {
       $output .= '<a class="facetwp-page button--outline button--outline--gray alignright" data-page="' . ($page + 1) . '">Next</a>';
   }
   return $output;

df9e037dbfe7e71beef24bd64fd30da42ca646fa
Kelli Monahan <kmonahan@bluestatedigital.com>
2016-08-04 09:55:57 -0500

    Remove post type label on programs page

--- wp-content/themes/guny/facetwp-program.php
+++ wp-content/themes/guny/facetwp-program.php
@@ -2,5 +2,5 @@
 $context = Timber::get_context();
 $posts = Timber::get_posts();
 $context['posts'] = $posts;
-$templates = array( 'partials/post-list.twig' );
+$templates = array( 'partials/program-list.twig' );
 Timber::render( $templates, $context );

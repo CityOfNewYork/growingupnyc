e49a40607b82879310b82d28dee6d4372729dd3d
Kelli Monahan <kmonahan@bluestatedigital.com>
2016-08-08 08:59:00 -0500

    Remove unused meta box

--- wp-content/themes/guny/includes/guny_meta_boxes.php
+++ wp-content/themes/guny/includes/guny_meta_boxes.php
@@ -7,3 +7,11 @@ function guny_remove_meta() {
   remove_meta_box( 'age_groupdiv', 'age', 'side' );
 }
 add_action( 'admin_menu', 'guny_remove_meta' );
+
+/**
+* Remove Event Options metabox from events
+*/
+function guny_remove_event_options() {
+  remove_meta_box( 'tribe_events_event_options', 'tribe_events', 'side' );
+}
+add_action( 'add_meta_boxes_tribe_events', 'guny_remove_event_options' );

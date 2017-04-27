3922940df991501152711288725c64d4e4104001
Amalan Premkumar <amalanpremkumar@gmail.com>
2017-04-27 10:01:07 +0530

    Search issue fully fixed.

--- wp-content/themes/guny/assets/js/source.dev.js
+++ wp-content/themes/guny/assets/js/source.dev.js
@@ -2007,10 +2007,22 @@ if (objCtr.defineProperty) {
 	});
 
 	exports.default = function () {
+	  function getURLParameter(name) {
+	    return decodeURIComponent((new RegExp('[?|&]' + name + '=([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
+	  }
+
 	  if (typeof window.FWP !== 'undefined' && $('body').hasClass('page-template-template-search')) {
 	    $('.facetwp-facet-search').on('click', '.facetwp-searchbtn', function (event) {
 	      event.preventDefault();
 	      window.FWP.autoload();
+	    });
+
+	    $(document).ajaxComplete(function () {
+	      if (getURLParameter("exactvalue")) {
+	        var searchvalue = getURLParameter("fwp_search");
+	        $('.facetwp-search').val('"' + searchvalue + '"');
+	        window.history.pushState('object or string', 'Title', '/search/?fwp_search="' + searchvalue + '"');
+	      }
 	    });
 	  }
 	};

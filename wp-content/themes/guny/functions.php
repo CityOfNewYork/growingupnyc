2d404d1e196e8f799009061d442fd2b78f908345
Amalan Premkumar <amalanpremkumar@gmail.com>
2017-05-08 12:25:25 +0530

    Adding the maps into trip page.

--- wp-content/themes/guny/functions.php
+++ wp-content/themes/guny/functions.php
@@ -433,6 +433,22 @@ function microsite_styles() {
     wp_enqueue_style( 'magazine', get_stylesheet_directory_uri() . '/magazine.css', null, '0.1' ); 
   } 
 }
+
+function google_map_theme_add_scripts() {
+  // wp_enqueue_script( 'google-map', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDrvNnQZBiASAH3JI7LNFewrX9jeYZlMWo', array(), '3', true );
+  wp_enqueue_script( 'google-map-init', get_template_directory_uri() . '/library/js/google-maps.js', array('google-map', 'jquery'), '0.1', true );
+}
+
+add_action( 'wp_enqueue_scripts', 'google_map_theme_add_scripts' );
+
+// function my_acf_google_map_api( $api ){
+  
+//   $api['key'] = 'AIzaSyDrvNnQZBiASAH3JI7LNFewrX9jeYZlMWo';
+  
+//   return $api;
+  
+// }
+// add_filter('acf/fields/google_map/api', 'my_acf_google_map_api');
 //End of the codes Amalan
 
 // Customize TinyMCE settings

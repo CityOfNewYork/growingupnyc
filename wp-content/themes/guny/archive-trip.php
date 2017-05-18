a6cc44192ef79b7c0c79d67360d3e1670ed3e88a
Amalan Premkumar <amalanpremkumar@gmail.com>
2017-05-18 09:19:21 +0530

    Adding trip landing page

--- wp-content/themes/guny/archive-trip.php
+++ wp-content/themes/guny/archive-trip.php
@@ -13,9 +13,7 @@ if ( ! class_exists( 'Timber' ) ) {
 $context = Timber::get_context();
 $context['posts'] = facetwp_display('template', 'trips');
 $context['pagination'] = facetwp_display('pager');
-$context['facet_program_type'] = facetwp_display( 'facet', 'trip_type' );
+$context['facet_trip_type'] = facetwp_display( 'facet', 'trip_type' );
 $context['facet_ages'] = facetwp_display( 'facet', 'ages' );
 $templates = array( 'list-trip.twig', 'microsite-list.twig' );
-// echo "SALOG : archieve trip php";
 Timber::render( $templates, $context );
-// print_r($context['posts']);

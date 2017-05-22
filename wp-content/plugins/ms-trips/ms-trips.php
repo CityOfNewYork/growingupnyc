adbcab8009040e24164acf82d24131933c49996b
Amalan Premkumar <amalanpremkumar@gmail.com>
2017-05-22 11:37:46 +0530

    adding final updates on the landing pages.

--- wp-content/plugins/ms-trips/ms-trips.php
+++ wp-content/plugins/ms-trips/ms-trips.php
@@ -7,33 +7,31 @@ Description: Trips posttypes for Microsite
 */
 
 function ms_trips_create() {
-    // set up labels
+  // set up labels
   $labels = array(
     'name' => 'Trips',
-      'singular_name' => 'Trip',
-      'add_new' => 'Add New Trip',
-      'add_new_item' => 'Add New Trip',
-      'edit_item' => 'Edit Trip',
-      'new_item' => 'New Trip',
-      'all_items' => 'All Trips',
-      'view_item' => 'View Trip',
-      'search_items' => 'Search Trips',
-      'not_found' =>  'No Trips Found',
-      'not_found_in_trash' => 'No Trips found in Trash', 
-      'parent_item_colon' => '',
-      'menu_name' => 'Trips',
+    'singular_name' => 'Trip',
+    'add_new_item' => 'Add New Trip',
+    'edit_item' => 'Edit Trip',
+    'new_item' => 'New Trip',
+    'view_item' => 'View Trip',
+    'search_items' => 'Search Trips',
+    'not_found' =>  'No Trips Found',
+    'not_found_in_trash' => 'No Trips found in Trash',
+    'all_items' => 'All Trips',
+    'archives' => 'Trip Archives',
+    'insert_into_item' => 'Insert into trip',
+    'uploaded_to_this_item' => 'Uploaded to this trip'
     );
 
-      //register post type
+  //register post type
   register_post_type( 'trip', array(
     'labels' => $labels,
-    'has_archive' => true,
     'public' => true,
     'menu_position' => 24,
     'menu_icon' => 'dashicons-groups',
     'supports' => array( 'title', 'excerpt', 'thumbnail'),
-    'exclude_from_search' => false,
-    'capability_type' => 'post',
+    'has_archive' => true,
     'rewrite' => array(
         'slug' => 'trips',
         'with_front' => false
@@ -50,15 +48,6 @@ function ms_trips_create() {
         'slug' => 'trip-group',
         'with_front' => false
       ),
-      'hierarchical' => true
-    )
-  );
-
-  register_taxonomy(
-    'trips_cat',
-    'trip',
-    array(
-      'label' => __( 'Trip Categories' ),
       'hierarchical' => true
     )
   );
--- wp-content/themes/guny/archive-trip.php
+++ wp-content/themes/guny/archive-trip.php
@@ -13,7 +13,17 @@ if ( ! class_exists( 'Timber' ) ) {
 $context = Timber::get_context();
 $context['posts'] = facetwp_display('template', 'trips');
 $context['pagination'] = facetwp_display('pager');
+$context['facet_program_type'] = facetwp_display( 'facet', 'program_type' );
 $context['facet_trip_type'] = facetwp_display( 'facet', 'trip_type' );
 $context['facet_ages'] = facetwp_display( 'facet', 'ages' );
 $templates = array( 'list-trip.twig', 'microsite-list.twig' );
+
+// $context = Timber::get_context();
+// $context['posts'] = facetwp_display('template', 'programs');
+// $context['pagination'] = facetwp_display('pager');
+// $context['facet_program_type'] = facetwp_display( 'facet', 'program_type' );
+// $context['facet_ages'] = facetwp_display( 'facet', 'ages' );
+// $context['facet_trip_type'] = facetwp_display( 'facet', 'trip_type' );
+// $templates = array( 'list-program.twig', 'list.twig' );
+
 Timber::render( $templates, $context );

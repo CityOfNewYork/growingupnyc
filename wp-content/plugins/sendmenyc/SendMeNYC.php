36ec9558f91d170e5c3357047b6fa6709e04b828
Amalan Premkumar <amalanpremkumar@gmail.com>
2017-06-13 17:42:17 +0530

    removing C from setting page.

--- wp-content/plugins/sendmenyc/SendMeNYC.php
+++ wp-content/plugins/sendmenyc/SendMeNYC.php
@@ -34,8 +34,8 @@ add_filter( 'plugin_action_links_'.plugin_basename( __FILE__ ), '\SMNYC\settings
 add_action( 'admin_menu', '\SMNYC\add_settings_page' );
 function add_settings_page() {
 	add_options_page(
-		'GrowingUpNYCSms Settings',
-		'GrowingUpNYCSms',
+		'GrowingUpNYSms Settings',
+		'GrowingUpNYSms',
 		'manage_options',
 		'smnyc_config',
 		'\SMNYC\settings_content'
@@ -43,7 +43,7 @@ function add_settings_page() {
 }
 function settings_content(){ ?>
 <div class="wrap">
-	<h1>GrowingUpNYCSms Settings</h1>
+	<h1>GrowingUpNYSms Settings</h1>
 
 	<form method="post" action="options.php">
 		<?php

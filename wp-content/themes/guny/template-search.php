c2449a52652c930d7341ef4d48d826539bbfd0bd
Amalan Premkumar <amalanpremkumar@gmail.com>
2017-06-19 17:32:42 +0530

    Removing test commit

--- wp-content/themes/guny/template-search.php
+++ wp-content/themes/guny/template-search.php
@@ -14,7 +14,4 @@ $context['facet_post_type'] = facetwp_display( 'facet', 'post_type' );
 $context['pagination'] = facetwp_display('pager');
 //$context['did_you_mean'] = relevanssi_didyoumean(get_search_query(), '', '');
 $templates = array( 'search.twig', 'index.twig' );
-
-
-print_r(facetwp_display('template', 'default'));
 Timber::render( $templates, $context );

046ef21db1e36c6ea266038ec6477ec314f43c7e
Amalan Premkumar <amalanpremkumar@gmail.com>
2017-06-20 10:32:21 +0530

    Addin final tweeks.

--- wp-content/themes/guny/assets/js/source.dev.js
+++ wp-content/themes/guny/assets/js/source.dev.js
@@ -2187,13 +2187,22 @@ if (objCtr.defineProperty) {
 	        var viewCount = _jsCookie2.default.get('screenerViews') ? parseInt(_jsCookie2.default.get('screenerViews'), 10) : 1;
 	        if (viewCount >= 5) {
 	          _this._initRecaptcha();
-	          viewCount = 0;
+	          // viewCount = 0;
 	        }
 	        console.log(viewCount);
 	        // `2/1440` sets the cookie to expire after two minutes.
 	        _jsCookie2.default.set('screenerViews', ++viewCount, { expires: 2 / 1440 });
 	      });
 
+	      // // Determine whether or not to initialize ReCAPTCHA. This should be
+	      // // initialized only on every 10th view which is determined via an
+	      // // incrementing cookie.
+	      var viewCount = _jsCookie2.default.get('screenerViews') ? parseInt(_jsCookie2.default.get('screenerViews'), 10) : 1;
+	      if (viewCount >= 5) {
+	        this._initRecaptcha();
+	        // viewCount = 0;
+	      }
+	      console.log(viewCount);
 	      this._initialized = true;
 	      return this;
 	    }

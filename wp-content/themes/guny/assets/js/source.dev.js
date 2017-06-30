f4ffc64fde77af36663dc7450353dc89918e2799
mohomedanees <anees@thesmallaxxe.com>
2017-06-30 11:11:53 +0530

    section highlighter edits.

--- wp-content/themes/guny/assets/js/source.dev.js
+++ wp-content/themes/guny/assets/js/source.dev.js
@@ -1998,7 +1998,7 @@ if (objCtr.defineProperty) {
 	  //var eTop = $('#free-day-trips').offset().top;
 
 	  $sections.each(function () {
-	    sectionIdTonavigationLink[$(this).attr('id')] = $('.js-section-set > li > a[href=\\#' + $(this).attr('id') + ']');
+	    sectionIdTonavigationLink[$(this).attr('id')] = $('.js-section-set > li > a[href^=\#' + $(this).attr('id') + ']');
 	  });
 
 	  function optimized() {

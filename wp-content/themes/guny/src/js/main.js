dfd0f4aa977d9fd080223d48a147222064a9c1b9
devowhippit <devonhirth@gmail.com>
2018-01-31 11:41:12 -0500

    Removing deprecated js dependencies

--- wp-content/themes/guny/src/js/main.js
+++ wp-content/themes/guny/src/js/main.js
@@ -1,16 +1,10 @@
-import globalSearch from './modules/globalSearch.js';
-/* eslint-disable no-unused-vars */
-import toggleOpen from './modules/toggleOpen.js';
-/* eslint-enable no-unused-vars */
 import accordion from './modules/accordion.js';
 import simpleAccordion from './modules/simpleAccordion.js';
 import offcanvas from './modules/offcanvas.js';
 import overlay from './modules/overlay.js';
 import stickNav from './modules/stickNav.js';
-//import currentSection from './modules/currentSection.js';
 import sectionHighlighter from './modules/sectionHighlighter.js';
 import staticColumn from './modules/staticColumn.js';
-import searchResultsHeader from './modules/searchResultsHeader.js';
 import alert from './modules/alert.js';
 import bsdtoolsSignup from './modules/bsdtools-signup.js';
 import formEffects from './modules/formEffects.js';
@@ -21,6 +15,7 @@ import ShareForm from './modules/share-form.js';
 import captchaResize from './modules/captchaResize.js';
 import rotatingTextAnimation from './modules/rotatingTextAnimation.js';
 /* eslint-disable no-unused-vars */
+import toggleOpen from './modules/toggleOpen.js';
 import toggleMenu from './modules/toggleMenu.js';
 /* eslint-enable no-unused-vars */
 
@@ -33,21 +28,19 @@ function ready(fn) {
 }
 
 function init() {
-  globalSearch();
   toggleOpen('is-open');
   alert('is-open');
   offcanvas();
   accordion();
   simpleAccordion();
   overlay();
-  // Search results page
-  searchResultsHeader();
+
   // FacetWP pages
   facets();
+
   // Homepage
   staticColumn();
   stickNav();
-  //currentSection();
   bsdtoolsSignup();
   formEffects();
   owlSettings();
--- wp-content/themes/guny/webpack.config.js
+++ wp-content/themes/guny/webpack.config.js
@@ -37,5 +37,6 @@ module.exports = {
       $: 'jquery',
       jQuery: 'jquery'
     })
-  ]
+  ],
+  devtool: 'inline-source-map'
 };

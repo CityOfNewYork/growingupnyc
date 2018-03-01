0ac3870fd458f3133339713087a2417b2f4f0004
devowhippit <devonhirth@gmail.com>
2018-03-01 18:20:09 -0500

    Creating generic microsite template, remove contact form from section flexible content GNY-725

--- wp-content/themes/guny/template-generic-microsite-page.php
+++ wp-content/themes/guny/template-generic-microsite-page.php
@@ -10,5 +10,6 @@ $post = Timber::get_post();
 $templates = array( 'generic-microsite-page.twig' );
 
 $context['post'] = $post;
+$context['sections'] = Templating\get_sections();
 
 Timber::render( $templates, $context );

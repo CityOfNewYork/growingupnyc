d7679fb6ecc3e7f761c9395b5d71167988f1690a
devowhippit <devonhirth@gmail.com>
2018-03-02 14:58:44 -0500

    Add markdown instructions to location description field. Markdown composer module

--- wp-content/themes/guny/includes/summer_guides.php
+++ wp-content/themes/guny/includes/summer_guides.php
@@ -6,17 +6,68 @@
 
 namespace SummerGuides;
 
+/**
+ * Dependencies
+ */
+
+use Michelf\Markdown;
+
 
 /**
  * Constants
  */
 
+// The post type slug
 const SLUG = 'summer-guide';
 
+// The translation domain for WPML
 const TRANSLATION_DOMAIN = 'guny-summer-guide';
+
+// Custom field IDs
+const FIELD_CALL_TO_ACTION = 'field_5a972d6549e96';
+
+const FIELD_LOCATION_DESCRIPTION = 'field_5a972d0ffd99e';
+
+const FIELD_DATE_OPTIONS = 'field_5a9727557ffee';
+
+const FIELD_SECTIONS = 'field_5a78cc2bc5e51';
+
 
 
 /**
  * Functions
  */
 
+/**
+ * Get the call to action custom field
+ * @return [type] [description]
+ */
+function get_call_to_action() {
+  return get_field(FIELD_CALL_TO_ACTION);
+}
+
+/**
+ * [get_location_description description]
+ * @return [type] [description]
+ */
+function get_location_description() {
+  return Markdown::defaultTransform(
+    get_field(FIELD_LOCATION_DESCRIPTION)
+  );
+}
+
+/**
+ * [get_date_options description]
+ * @return [type] [description]
+ */
+function get_date_options() {
+  return get_field(FIELD_DATE_OPTIONS);
+}
+
+/**
+ * [get_sections description]
+ * @return [type] [description]
+ */
+function get_sections() {
+  return get_field(FIELD_SECTIONS);
+}
--- wp-content/themes/guny/vendor/composer/autoload_static.php
+++ wp-content/themes/guny/vendor/composer/autoload_static.php
@@ -6,9 +6,25 @@ namespace Composer\Autoload;
 
 class ComposerStaticInitc5487575bb73ac5e30ff681125bf5a64
 {
+    public static $prefixLengthsPsr4 = array (
+        'M' => 
+        array (
+            'Michelf\\' => 8,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Michelf\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/michelf/php-markdown/Michelf',
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInitc5487575bb73ac5e30ff681125bf5a64::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitc5487575bb73ac5e30ff681125bf5a64::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }

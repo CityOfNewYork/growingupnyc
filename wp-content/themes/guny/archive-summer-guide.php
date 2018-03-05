b0007924ed0d699760fc6004a47ab4f22bb58bba
devowhippit <devonhirth@gmail.com>
2018-03-05 12:02:15 -0500

    Add language switcher to summer guides

--- wp-content/themes/guny/archive-summer-guide.php
+++ wp-content/themes/guny/archive-summer-guide.php
@@ -8,4 +8,12 @@ $context = Timber::get_context();
 
 $templates = array('list-summer-guide.twig');
 
+$context['custom_switcher'] = Timber::compile(
+  array('partials/language-switcher.twig'),
+  array(
+    'languages' => Wpml\get_wpdb_languages(),
+    'current' => ICL_LANGUAGE_CODE
+  )
+);
+
 Timber::render($templates, $context);
--- wp-content/themes/guny/single-summer-guide.php
+++ wp-content/themes/guny/single-summer-guide.php
@@ -9,6 +9,14 @@ $post = Timber::get_post();
 
 $context['post'] = $post;
 
+$context['custom_switcher'] = Timber::compile(
+  array('partials/language-switcher.twig'),
+  array(
+    'languages' => Wpml\get_wpdb_languages(),
+    'current' => ICL_LANGUAGE_CODE
+  )
+);
+
 $templates = array('single-' . $post->post_type . '.twig');
 
 Timber::render($templates, $context);

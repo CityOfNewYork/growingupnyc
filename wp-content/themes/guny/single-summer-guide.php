b51f2e09d61889daff366d43e82866dca009ac5d
devowhippit <devonhirth@gmail.com>
2018-05-09 15:23:12 -0400

    Setting events link to context

--- wp-content/themes/guny/single-summer-guide.php
+++ wp-content/themes/guny/single-summer-guide.php
@@ -21,5 +21,6 @@ $context['taxonomies'] = SummerGuides\get_taxonomies();
 $context['sections'] = Templating\get_sections();
 $context['archive_link'] = SummerGuides\get_archive_link();
 $context['domain'] = SummerGuides\get_translation_domain();
+$context['events_link'] = get_post_type_archive_link('tribe_events');
 
 Timber::render(array('single-summer-guide.twig'), $context);

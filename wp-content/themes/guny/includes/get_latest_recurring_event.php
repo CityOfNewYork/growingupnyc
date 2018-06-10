9cc6344df3d8d53f67d32e6ee5f0fa377e6000b5
kimpenguin <kimmypeng@gmail.com>
2018-06-09 20:49:21 -0400

    getting the single one-off related events for summer

--- wp-content/themes/guny/includes/get_latest_recurring_event.php
+++ wp-content/themes/guny/includes/get_latest_recurring_event.php
@@ -10,27 +10,32 @@ use GunyEvent;
  * @param  number $number  The number of posts to retrieve
  * @return array           Collection of posts, false if tribe_events are disabled
  */
-function get_latest_recurring_event($ID, $number = 1) {
+
+function get_latest_recurring_event($section_events) {
+  // empty array for events
+  $events=array();
   if (!function_exists('tribe_get_events')) return false;
 
-  // If it is a recurring event but not the parent post
-  if (tribe_is_recurring_event($ID) && wp_get_post_parent_id($ID) !== 0) {
-    // Get the parent of the post
-    $ID = wp_get_post_parent_id($ID);
+  foreach ($section_events as &$value) {
+    $event = array(
+      'p' => $value['id'],
+      'eventDisplay' => 'list',
+      'meta_query'=> array(
+        array(
+          'key' => '_EventStartDate',
+          'compare' => '>=', 
+          'value' => date('Y-m-d H:i:s'),
+          'type' => 'DATE'
+        )
+      ),
+    );
+    if(tribe_get_events( $event )){
+      array_push($events, tribe_get_events( $event ));
+    }
   }
-
-  $events = tribe_get_events(array(
-    'post_parent' => $ID,
-    'meta_key' => '_EventStartDate',
-    'orderby' => '_EventStartDate',
-    'order' => 'ASC',
-    'posts_per_page' => $number,
-    'start_date' => date('Y-m-d H:i:s')
-  ));
-
   foreach($events as $i => $event) {
-    $events[$i] = new GunyEvent($events[$i]);
+    $events[$i] = new GunyEvent($events[$i][0]);
   }
-
   return $events;
 }
+

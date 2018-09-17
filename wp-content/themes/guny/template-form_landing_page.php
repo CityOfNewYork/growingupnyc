3dec7b1a79da2f6c042afa0106e1f981b004678b
kimpenguin <kimmypeng@gmail.com>
2018-09-17 17:28:06 -0400

    moving checkboxes and adding bottom padding to field group

--- wp-content/themes/guny/template-form_landing_page.php
+++ wp-content/themes/guny/template-form_landing_page.php
@@ -8,6 +8,11 @@ $context = Timber::get_context();
 $post = Timber::get_post();
 
 $context['post'] = $post;
+// echo '<pre>',var_dump($post),'</pre>';
+// echo '<pre>',$post->post_parent,'</pre>';
+if( $post->post_parent > 0 ) {
+	$context['parent'] = true;
+}
 
 $templates = array( 'form-landing-page.twig' );
 Timber::render( $templates, $context );

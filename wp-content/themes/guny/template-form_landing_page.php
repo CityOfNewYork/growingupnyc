166ac8a54e209972ac2dd0184f123bd0381795e8
kimpenguin <kimmypeng@gmail.com>
2018-09-18 16:30:36 -0400

    moving the header and description outside form and using feature image

--- wp-content/themes/guny/template-form_landing_page.php
+++ wp-content/themes/guny/template-form_landing_page.php
@@ -8,6 +8,8 @@ $context = Timber::get_context();
 $post = Timber::get_post();
 
 $context['post'] = $post;
+$featured_image = get_the_post_thumbnail_url($post);
+$context['featured_image'] = $featured_image;
 
 $templates = array( 'form-landing-page.twig' );
 Timber::render( $templates, $context );

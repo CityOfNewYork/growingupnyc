842a6138929520cad2937f7f4141185d54d4e08b
KISS <me@luisam.com>
2018-11-08 14:01:52 -0500

    GNY-968_404-dyanmic-markup

--- wp-content/themes/guny/404.php
+++ wp-content/themes/guny/404.php
@@ -6,8 +6,10 @@
 $context = Timber::get_context();
 $context['top_widget'] = Timber::get_widgets('top_widget');
 
-$post_id= icl_object_id(get_page_by_title( '404' )->ID, 'page', FALSE, ICL_LANGUAGE_CODE);
+$post_id = icl_object_id(get_page_by_title( '404' )->ID, 'page', FALSE, ICL_LANGUAGE_CODE);
 $post = get_page($post_id);
-$context['post']=$post;
+
+$context['post'] = $post;
+$context['side_menu_categories'] = get_field('side_menu_categories', $post->id);
 
 Timber::render(array('404.twig'), $context);

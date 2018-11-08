2ba29e9c98789325d38fbdfe9764151ab0e5c412
kimpenguin <kimmypeng@gmail.com>
2018-11-08 12:47:09 -0500

    adding get_page by correct language for 404

--- wp-content/themes/guny/404.php
+++ wp-content/themes/guny/404.php
@@ -4,8 +4,10 @@
 */
 
 $context = Timber::get_context();
+$context['top_widget'] = Timber::get_widgets('top_widget');
 
-$post=get_page_by_title( '404 page' );
+$post_id= icl_object_id(get_page_by_title( '404' )->ID, 'page', FALSE, ICL_LANGUAGE_CODE);
+$post = get_page($post_id);
 $context['post']=$post;
 
 Timber::render(array('404.twig'), $context);

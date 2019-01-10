d06d76d5ceb3e3c255c6820dde272635b9ce174f
kimpenguin <kpeng@nycopportunity.nyc.gov>
2019-01-10 13:30:39 -0500

    removing condition on google maps enqueue and rebuilding scripts

--- wp-content/themes/guny/functions.php
+++ wp-content/themes/guny/functions.php
@@ -141,10 +141,8 @@ class GunySite extends TimberSite {
       wp_enqueue_script( 'modernizr', get_template_directory_uri() . '/assets/js/modernizr.js', array(), '3.0.0', false );
       wp_enqueue_script( 'jquery', get_template_directory_uri() . '/src/js/vendor/jquery.js', array(), '2.1.14', false );
       wp_enqueue_script( 'owl-js', get_template_directory_uri() . '/src/js/vendor/owl.carousel.min.js', array(), '2.2.1', true );
-      if( explode("/",$_SERVER["REQUEST_URI"])[1] == 'generationnyc'){
-        wp_enqueue_script( 'google-map', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCt1i2Y7wnOQooLwKhxKqbe6IWJl02dtyM', array(), '3', true );
-        wp_enqueue_script( 'google-map-init', get_template_directory_uri() . '/src/js/vendor/google-maps.js', array('google-map', 'jquery'), '0.1', true );
-      }
+      wp_enqueue_script( 'google-map', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCt1i2Y7wnOQooLwKhxKqbe6IWJl02dtyM', array(), '3', true );
+      wp_enqueue_script( 'google-map-init', get_template_directory_uri() . '/src/js/vendor/google-maps.js', array('google-map', 'jquery'), '0.1', true );
     }
   }
 

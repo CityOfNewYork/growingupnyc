37dc5cf82851b8b87c7d4a5e88c848817d069a36
kimpenguin <kpeng@nycopportunity.nyc.gov>
2020-10-28 14:10:00 -0400

    updating dependencies and replacing timber plugin

--- vendor/aws/aws-sdk-php/src/Sdk.php
+++ vendor/aws/aws-sdk-php/src/Sdk.php
@@ -471,7 +471,7 @@ namespace Aws;
  */
 class Sdk
 {
-    const VERSION = '3.158.11';
+    const VERSION = '3.158.15';
 
     /** @var array Arguments for creating clients */
     private $args;
--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -7,6 +7,7 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'AltoRouter' => $vendorDir . '/altorouter/altorouter/AltoRouter.php',
+    'Attribute' => $vendorDir . '/symfony/polyfill-php80/Resources/stubs/Attribute.php',
     'Stringable' => $vendorDir . '/symfony/polyfill-php80/Resources/stubs/Stringable.php',
     'UnhandledMatchError' => $vendorDir . '/symfony/polyfill-php80/Resources/stubs/UnhandledMatchError.php',
     'ValueError' => $vendorDir . '/symfony/polyfill-php80/Resources/stubs/ValueError.php',

7e9f046dc99dc5fd853ad0494a6e8f6f5be3221e
Yonas Fesehatsion <yone920@gmail.com>
2021-06-09 10:49:03 -0400

    v4.2.2

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,15 +22,13 @@ class ComposerAutoloaderInit3c0c47bdd559dc86d448f15792196e85
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit3c0c47bdd559dc86d448f15792196e85', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit3c0c47bdd559dc86d448f15792196e85', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require __DIR__ . '/autoload_static.php';
+            require_once __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInit3c0c47bdd559dc86d448f15792196e85::getInitializer($loader));
         } else {
